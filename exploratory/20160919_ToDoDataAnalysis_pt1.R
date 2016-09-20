0e7ecbbb7641570f80566d4e44afcd41f9bd024d
marcsze <marcsze@med.umich.edu>
2016-09-20 15:26:49 -0400

    update to do list items

--- exploratory/20160919_ToDoDataAnalysis_pt1.R
+++ exploratory/20160919_ToDoDataAnalysis_pt1.R
@@ -2,10 +2,18 @@
 ## Focus strictly on Lesion, SRNLesion, and three groups (Normal, Adenoma, Cancer) classifications
 ## Marc Sze
 
+# Load required dependencies and libraries
+source('code/functions.R')
+source('code/graphFunctions.R')
+
+loadLibs(c("pROC","randomForest","AUCRF", "Boruta", "dplyr", "tidyr", "ggplot2", "reshape2", 
+           "gridExtra", "scales", "wesanderson", "vegan"))
+
 
 ## Need to look at differences between initial and follow up between microbiome (thetayc) and fit.
 # Does the microbiome show a difference in community versus the fit?
 
+metaI <- read.delim('data/process/initials_metadata.tsv', header=T, sep='\t')
 metaF <- read.delim('data/process/followUps_metadata.txt', header=T, sep='\t') %>% mutate(lesion = factor(NA, levels=c(0,1)))
 thetaCompTotal <- dissplit('data/process/followUps.final.an.unique_list.thetayc.0.03.lt.ave.dist',split=F, meta = F)
 
@@ -45,13 +53,41 @@ grid.arrange(
 )
 
 
+## Need to look at how the follow up samples look like versus their initial samples and then visualize versus
+## the normal values
+
+breakDown_samples <- c(rep("initial", length(rownames(metaF))), rep("follow_up", length(rownames(metaF))), 
+                       rep("normal", length(metaI$sample[metaI$dx == "normal"])))
+
+theta_init_follow_dist <- pickDistanceValues(
+  as.character(c(metaF$initial, metaF$followUp, metaI$sample[metaI$dx == "normal"])), 
+  as.character(c(metaF$initial, metaF$followUp, metaI$sample[metaI$dx == "normal"])), 
+  thetaCompTotal, withMeta = FALSE)
+
+set.seed(050416)
+adonis(as.dist(theta_init_follow_dist) ~ factor(breakDown_samples))
+
+thetayc.mds <- metaMDS(as.dist(theta_init_follow_dist)) %>% scores() %>% as.data.frame() %>% 
+  mutate(samples = factor(breakDown_samples))
+
+ggplot(data = thetayc.mds, aes(x=NMDS1, y=NMDS2)) + geom_point(aes(color=samples)) + theme_bw() + coord_equal() + 
+  stat_ellipse(aes(group = samples, color = samples, fill = samples), alpha = 0.25, geom = "polygon")
+
+
+## Need to look at how FIT measurements compare to normal individuals
+
+normalFit <- filter(metaI, dx == "normal") %>% select(fit_result)
+
+initial_Fit_Pvalue <- wilcox.test(normalFit$fit_result, metaF$initial)$p.value
+followUp_Fit_Pvalue <- wilcox.test(normalFit$fit_result, metaF$fit_followUp)$p.value
+
+
 ## Need to try the opposite and look at what RF and Borutat would pull out when looking solely at those with 
 ## follow up
 
 ## Need to look into more detail into the differences between these models
 
-## Need to look at how FIT measurements compare to a random sampling of normal individuals
-# Probably need to bootstrap this
+
 
 
 
