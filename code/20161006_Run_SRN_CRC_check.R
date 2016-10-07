d814dc4d56c7da201c1325866e27ddfba1036b30
marcsze <marcsze@med.umich.edu>
2016-10-07 11:05:24 -0400

    update modularized code

--- code/20161006_Run_SRN_CRC_check.R
+++ code/20161006_Run_SRN_CRC_check.R
@@ -17,9 +17,61 @@ woFit_data <- read.csv("results/tables/noFIT.models.datatable.csv", header = T, 
 withFit_cutoffs <- read.csv("results/tables/withFIT.cutoffs.csv", header = T, stringsAsFactors = F)
 woFit_cutoffs <- read.csv("results/tables/noFIT.cutoffs.csv", header = T, stringsAsFactors = F)
 
+tax <- read.delim('data/process/followUps.final.an.unique_list.0.03.cons.taxonomy', 
+                  sep='\t', header=T, row.names=1)
+shared <- read.delim('data/process/followUps.final.an.unique_list.0.03.subsample.0.03.filter.shared', 
+                     header=T, sep='\t')
 
-length(rownames(filter(withFit_data, L1 == "SRNlesion_rf_opt", time_point == "initial", detailed_diagnosis == "Cancer", 
-               positive > withFit_cutoffs$cutoff[1]))) 
+# Convert taxa table to a data frame with columns for each taxonomic division
+tax_df <- data.frame(do.call('rbind', strsplit(as.character(tax$Taxonomy), ';')))
+rownames(tax_df) <- rownames(tax)
+colnames(tax_df) <- c("Domain", "Phyla", "Order", "Class", "Family", "Genus")
+tax_df <- as.data.frame(apply(tax_df, 2, function(x) gsub("\\(\\d{2}\\d?\\)", "", x)))
+rm(tax)
+# This uses the R regex to remove numerals and the brackets the \\ is used to escape specific function components
+
+# Generate COunt data for with and wo Fit
+WF_modelCountList <- list(
+  SRNModel = createCountTable(withFit_data, withFit_cutoffs$cutoff[1], 
+                              "SRNlesion_rf_opt", c("initial", "followup"), c("Cancer", "adv Adenoma", "Adenoma")), 
+  LesionModel = createCountTable(withFit_data, withFit_cutoffs$cutoff[3], 
+                                 "lesion_rf_opt", c("initial", "followup"), c("Cancer", "adv Adenoma", "Adenoma"))
+)
+
+
+WoF_modelCountList <- list(
+  SRNModel = createCountTable(woFit_data, woFit_cutoffs$cutoff[1], 
+                              "SRNlesion_rf_opt", c("initial", "followup"), c("Cancer", "adv Adenoma", "Adenoma")), 
+  LesionModel = createCountTable(woFit_data, woFit_cutoffs$cutoff[3], 
+                                 "lesion_rf_opt", c("initial", "followup"), c("Cancer", "adv Adenoma", "Adenoma"))
+)
+
+
+groupList <- c("Cancer", "adv Adenoma", "Adenoma")
+LM_WF_pvalues <- c()
+LM_WoF_pvalues <- c()
+
+for(i in 1:(length(groupList))){
+  
+  LM_WF_pvalues <- c(LM_WF_pvalues, runFisherTest(WF_modelCountList[["LesionModel"]], 
+                                                  Group_inv = paste(groupList[i]), side = "greater")$p.value)
+  
+  LM_WoF_pvalues <- c(LM_WoF_pvalues, runFisherTest(WoF_modelCountList[["LesionModel"]], 
+                                                   Group_inv = paste(groupList[i]), side = "greater")$p.value)
+  
+  
+  
+}
+
+pvalue_table <- as.data.frame(cbind(c("Cancer", "advAdenoma", "Adenoma"), 
+                      LM_WF_pvalues, WF_padjust = p.adjust(LM_WF_pvalues, method = "bonferroni"), 
+                      LM_WoF_pvalues, WoF_padjust = p.adjust(LM_WoF_pvalues, method = "bonferroni")), stringsAsFactors = F)
+
+colnames(pvalue_table)[1] <- "Disease"
+
+write.csv(pvalue_table, "results/tables/lesion_WF_IF_pvalue_summary.csv", row.names = F)
+write.csv(WF_modelCountList[["LesionModel"]], "results/tables/lesion_PosCount_WF_IF_summary.csv", row.names = F)
+write.csv(WoF_modelCountList[["LesionModel"]], "results/tables/lesion_PosCount_WoF_IF_summary.csv", row.names = F)
 
 
 
