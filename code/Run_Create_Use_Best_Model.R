baf274d543b00c7b35eeb02f3e872c5b8d2583ce
marcsze <marcasze@gmail.com>
2016-12-06 08:28:06 -0500

    update code errors

--- code/Run_Create_Use_Best_Model.R
+++ code/Run_Create_Use_Best_Model.R
@@ -12,14 +12,14 @@ loadLibs(c("dplyr", "caret","scales", "wesanderson", "randomForest", "pROC"))
 
 # Read in necessary data frames
 
-test_data <- read.csv("results/tables/full_test_data.csv", header = TRUE)
+test_data <- read.csv("results/tables/full_test_data.csv", header = TRUE, row.names = 1)
 split_data_results <- read.csv("results/tables/ROC_model_summary.csv", header = TRUE, stringsAsFactors = F)
 test_data_roc <- read.csv("results/tables/test_data_roc.csv", header = TRUE, stringsAsFactors = F)
 auc_data_table <- read.csv("results/tables/auc_summary.csv", header = TRUE, row.names = 1)
 
 # Get best mtry to use
-
-maximized_mtry <- as.numeric(names(test[test == max(test)]))
+mtry_table <- table(split_data_results$best_mtry)
+maximized_mtry <- as.numeric(names(mtry_table[mtry_table == max(mtry_table)]))
 
 # Create Random Forest model
 
@@ -41,3 +41,82 @@ test_data_roc <- rbind(test_data_roc,
 
 write.csv(test_data_roc, 
           "results/tables/test_data_roc.csv", row.names = F)
+
+#load in metadata to get IDs to select shared file by
+good_metaf <- read.csv("results/tables/mod_metadata/good_metaf_final.csv", 
+                       stringsAsFactors = F, header = T)
+
+#create vector in correct order
+
+samples <- c(good_metaf$initial, good_metaf$followUp)
+
+#read in shared file and keep only the samples that are needed
+shared <- read.delim('data/process/final.0.03.subsample.shared', 
+                     header=T, sep='\t') %>% select(-label, -numOtus)
+rownames(shared) <- shared$Group
+shared <- shared[as.character(samples), ]
+
+# one sample has no follow up fit so need to remove that
+samplesToRemove <- filter(good_metaf, is.na(fit_followUp)) %>% 
+  select(initial, followUp)
+
+# Update good_metaf
+updated_metaf <- filter(
+  good_metaf, initial != samplesToRemove[, "initial"])
+
+
+# Remove the sample
+shared <- filter(
+  shared, Group != samplesToRemove[, "initial"], 
+  Group != samplesToRemove[, "followUp"])
+
+# Keep only OTUs in test data
+shared <- select(shared, Group, one_of(colnames(test_data)))
+
+
+# Load in test data set
+follow_up_data <- read.csv("results/tables/follow_up_prediction_table.csv")
+
+test_follow_up_data <- cbind(select(follow_up_data, -contains("Otu0")), 
+                   select(shared, -Group))
+
+
+# Make predictions on samples with follow up
+initial_predictions <- predict(full_model, 
+  newdata = test_data[1:(length(rownames(test_follow_up_data))/2), ], 
+  type='prob')
+
+followup_predictions <- predict(full_model, 
+  newdata = test_data[((length(rownames(
+    follow_up_data))/2)+1):length(rownames(test_follow_up_data)), ], 
+  type='prob')
+
+
+# Create data table needed for figure 4
+
+probability_data_table <- cbind(
+  No = c(initial_predictions[, "No"], followup_predictions[, "No"]), 
+  Yes = c(initial_predictions[, "Yes"], followup_predictions[, "Yes"]), 
+  sampleType = c(rep("initial", length(rownames(initial_predictions))), 
+                 rep("followup", length(rownames(followup_predictions)))), 
+  disease_free = rep(updated_metaf$Disease_Free, 2), 
+  diagnosis = rep(updated_metaf$Diagnosis, 2), 
+  Dx_Bin = rep(updated_metaf$Dx_Bin, 2), 
+  chemo = rep(updated_metaf$chemo_received, 2), 
+  rads = rep(updated_metaf$radiation_received, 2), 
+  EDRN = rep(updated_metaf$EDRN, 2))
+
+write.csv(probability_data_table, 
+          "results/tables/follow_up_probability_summary.csv", row.names = F)
+
+
+
+
+
+
+
+
+
+
+
+
