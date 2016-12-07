215cb6fc52e82ed2f765aa477f6f781cc064f05e
marcsze <marcasze@gmail.com>
2016-12-07 14:23:43 -0500

    add figure 6 baxter suggestion

--- code/Run_Figure6.R
+++ code/Run_Figure6.R
@@ -12,6 +12,7 @@ loadLibs(c("dplyr", "ggplot2", "reshape2", "gridExtra", "scales", "wesanderson",
 # Load needed data
 graph_data <- read.csv("results/tables/adn_crc_maybe_diff.csv", header = T, stringsAsFactors = F)
 
+graph_data <- mutate(graph_data, j_values = jitter(value))
 
 labs <- c(
   paste("Fusobacterium Nucleatum ", "(", unique(graph_data$otu)[1], ")", sep = ""), 
@@ -23,31 +24,49 @@ names(labs) <- unique(graph_data$otu)
 
 # Create the figure
 
-grid.arrange(  
-  # Adenoma OTUs graph
-  filter(graph_data, Diagnosis != "cancer") %>% 
-    ggplot(aes(factor(sampleType, levels = c("initial", "follow_up")), 
-               log10(jitteredValues + 1.1), group = factor(EDRN))) + 
-    geom_line(aes(color = factor(Disease_Free))) + geom_point(aes(color = factor(Disease_Free))) +  
-    facet_wrap(~variable, labeller = as_labeller(lesion_selected_labs)) + coord_cartesian(ylim = c(0, 4)) + 
-    theme_bw() + ylab("Log Total Sequences") + xlab("") + ggtitle("Adenoma") + 
-    scale_colour_manual(values = "blue") +  
-    theme(legend.position="none", plot.margin = unit(c(1, 1, 1, 1), "lines"), 
-          plot.title = element_text(size=20, face="bold")), 
+crc_specific <- grid.arrange(  
   # Cancer OTUs graph
-  filter(select_otu_data, Diagnosis == "adenocarcinoma" | Diagnosis == "N/D") %>% 
-    ggplot(aes(factor(sampleType, levels = c("initial", "follow_up")), 
-               log10(jitteredValues+1.1), group = factor(EDRN))) + 
+  filter(graph_data, Dx_Bin == "cancer") %>% 
+    ggplot(aes(factor(sampleType, levels = c("initial", "followup")), 
+               j_values, group = factor(EDRN))) + 
     geom_line(aes(color = factor(Disease_Free, levels = c("n", "y", "unknown")))) + 
     geom_point(aes(color = factor(Disease_Free, levels = c("n", "y", "unknown")))) + 
-    facet_wrap(~variable, labeller = as_labeller(lesion_selected_labs)) + coord_cartesian(ylim = c(0, 4)) + 
-    theme_bw() + ylab("Log Total Sequences") + xlab("") + ggtitle("Cancer") + 
-    scale_colour_manual(name = "Cancer Free", labels = c("No", "Yes", "Unknown"), values = c("darkred", "orange", "red")) + 
-    theme(legend.position=c(0.9,0.8), plot.margin = unit(c(1, 1, 1, 1), "lines"), 
-          plot.title = element_text(size=20, face="bold"), legend.title = element_text(face="bold")))
+    facet_wrap(~otu, labeller = as_labeller(labs)) + coord_cartesian(ylim = c(0, 0.105)) + 
+    theme_bw() + ylab("Relative Abundance") + xlab("") + ggtitle("A") + 
+    scale_colour_manual(name = "Cancer Free", 
+                        label = c("No", "Yes", "Unknown"),  
+                        values = wes_palette("GrandBudapest")) + 
+    scale_x_discrete(
+      breaks = c("initial", "followup"), 
+      labels = c("Initial", "Follow Up")) + 
+    theme(legend.title = element_text(face="bold"), 
+          legend.position = c(0.40, 0.30), 
+          plot.margin = unit(c(1, 1, 1, 1), "lines"), 
+          plot.title = element_text(size=20, face="bold")), 
+  
+  # Adenoma OTUs graph
+  filter(graph_data, Dx_Bin != "cancer") %>% 
+    ggplot(aes(factor(sampleType, levels = c("initial", "followup")), 
+               j_values, group = factor(EDRN))) + 
+    geom_line(aes(color = factor(Dx_Bin))) + geom_point(aes(color = factor(Dx_Bin))) +  
+    facet_wrap(~otu, labeller = as_labeller(labs)) + coord_cartesian(ylim = c(0, 0.002)) + 
+    theme_bw() + ylab("Relative Abundance") + xlab("") + ggtitle("B") + 
+    scale_colour_manual(name = "Polyp Type", values = c("cyan", "blue"), 
+                        breaks = c("adenoma", "adv_adenoma"), 
+                        labels = c("Adenoma", "SRN")) +  
+    scale_x_discrete(
+      breaks = c("initial", "followup"), 
+      labels = c("Initial", "Follow Up")) + 
+    theme(legend.title = element_text(face="bold"), 
+          legend.position = c(0.4, 0.30), 
+          plot.margin = unit(c(1, 1, 1, 1), "lines"), 
+          plot.title = element_text(size=20, face="bold"))
+
+  )
 
 
-
+ggsave(file = "results/figures/Figure6.pdf", crc_specific, 
+       width=8.5, height = 11, dpi = 300)
 
 
 
