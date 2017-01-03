1c106cd4d994390188b4408497a3c25b18f92e4c
marcsze <marcasze@gmail.com>
2017-01-03 09:08:01 -0500

    data structuring completion

--- code/Run_wilcoxson_all.R
+++ code/Run_wilcoxson_all.R
@@ -12,6 +12,27 @@ loadLibs(c("dplyr", "scales", "wesanderson", "ggplot2"))
 
 #Load needed data sets
 lesion_data <- read.csv("results/tables/full_test_data.csv", header = T, row.names = 1)
+shared <- read.delim('data/process/final.0.03.subsample.shared', 
+                     header=T, sep='\t') %>% select(Group, contains("Otu0"))
+
+metaf <- read.csv("results/tables/mod_metadata/good_metaf_final.csv", header = T, stringsAsFactors = F)
+
+#set row names for shared file
+rownames(shared) <- shared$Group
+
+#get OTUs to keep
+OTUs_to_keep <- colnames(select(lesion_data, contains("Otu0")))
+
+#get initial and follow up sample IDs (ALL, Adenoma Only, and CRC Only)
+ALL_ids_to_get <- c(metaf$initial, metaf$followUp)
+adn_ids_to_get <- c(filter(metaf, Diagnosis == "adenoma")[, "initial"], 
+                    filter(metaf, Diagnosis == "adenoma")[, "followUp"])
+
+crc_ids_to_get <- c(filter(metaf, Diagnosis != "adenoma")[, "initial"], 
+                    filter(metaf, Diagnosis != "adenoma")[, "followUp"])
+
+#subset the original shared file into an amenable testing formats
+shared_to_test_ALL <- shared[as.character(ALL_ids_to_get), OTUs_to_keep]
 
 
 
