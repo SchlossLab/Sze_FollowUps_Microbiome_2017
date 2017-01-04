5e15953742336d8e00dab554949be9b539976c0f
marcsze <marcasze@gmail.com>
2017-01-04 10:39:31 -0500

    first addition

---
+++ code/Run_reduced_best_model.R
@@ -0,0 +1,21 @@
+### Reduced model finalized model generation
+### get the best mtry and build final model with all data on reduced data set
+## Marc Sze
+
+
+#Load needed libraries
+source('code/functions.R')
+source('code/graphFunctions.R')
+
+loadLibs(c("dplyr", "caret","scales", "wesanderson", "randomForest", "pROC"))
+
+
+
+
+
+
+
+
+
+
+
