52bde9709b0d3720a3f53b149d58ce56ef05df86
marcsze <marcasze@gmail.com>
2017-01-04 07:20:03 -0500

    initial add

---
+++ code/Run_combine_aggregate_reduced_model.R
@@ -0,0 +1,17 @@
+### Reduced model finalized information
+### Combine different runs and get best model
+## Marc Sze
+
+###Load needed Libraries and functions
+source('code/functions.R')
+source('code/graphFunctions.R')
+
+loadLibs(c("dplyr", "ggplot2", "reshape2", "gridExtra", "scales", 
+           "wesanderson", "caret", "pROC"))
+
+
+
+
+
+
+
