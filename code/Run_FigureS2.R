cd26261699ec19d606906f22dda7e52ce8d47866
marcsze <marcasze@gmail.com>
2017-01-05 14:14:06 -0500

    add completed first half of figure (lesion portion)

--- code/Run_FigureS2.R
+++ code/Run_FigureS2.R
@@ -17,6 +17,7 @@ lesion_MDA_full <- read.csv("results/tables/lesion_model_top_vars_MDA_full_data.
 
 tax <- read.delim('data/process/final.taxonomy', sep='\t', header=T, row.names=1)
 
+occurance_data <- read.csv("results/tables/rf_wCV_imp_vars_summary.csv", header = T, stringsAsFactors = F)
 
 # Convert taxa table to a data frame with columns for each taxonomic division
 tax_df <- data.frame(do.call('rbind', strsplit(as.character(tax$Taxonomy), ';')))
@@ -25,16 +26,44 @@ colnames(tax_df) <- c("Domain", "Phyla", "Order", "Class", "Family", "Genus")
 tax_df <- as.data.frame(apply(tax_df, 2, function(x) gsub("\\(\\d{2}\\d?\\)", "", x)))
 rm(tax)
 
-ggplot(lesion_MDA_full, aes(factor(variables, 
+# Pull OTUs that are only in the MDA data
+OTU_IDs <- unique(filter(lesion_MDA_full, variables != "fit_result")[, "variables"])
+select_tax_df <- tax_df[OTU_IDs, ]
+low_tax_ID <- gsub("_unclassified", "", createTaxaLabeller(select_tax_df))
+
+# create labels for factor values with low taxonomy
+graph_labels <- c("FIT", paste(names(low_tax_ID), " (", low_tax_ID, ")", sep = ""))
+
+# align occurance data with MDA data
+rownames(occurance_data) <- occurance_data$Variable
+occurance_data <- occurance_data[unique(lesion_MDA_full$variables), ]
+
+
+# create part A plot
+lesionA <- ggplot(lesion_MDA_full, aes(factor(variables, 
                                    levels = rev(unique(lesion_MDA_full$variables)), 
-                                   labels = rev(unique(lesion_MDA_full$variables))), 
+                                   labels = rev(graph_labels)), 
                                    log10(value))) + 
-  geom_point(aes(color = variable)) + stat_summary(fun.y = "mean", colour = "black", geom = "point", size = 3) + 
-  coord_flip() + theme_bw() + ylab("Log10 MDA") + xlab("Variable") +  
-  theme(legend.position = "none")
+  geom_point(aes(color = variable)) + stat_summary(fun.y = "median", colour = "black", geom = "point", size = 3) + 
+  coord_flip() + theme_bw() + ylab("Log10 MDA") + xlab("Variable") +  ggtitle("A") + 
+  theme(plot.title = element_text(face = "bold", size = 32, hjust = 0), 
+        legend.position = "none", 
+        axis.title = element_text(face = "bold"), 
+        axis.text.y = element_text(size = 8))
+
+# create part B plot
+
+lesionB <- ggplot(occurance_data, aes(factor(Variable, 
+                                  levels = rev(unique(lesion_MDA_full$variables)), 
+                                  labels = rev(graph_labels)), total_appearance)) + 
+  geom_bar(stat = "identity") + coord_flip() + theme_bw() + 
+  ylab("Total Model Appearance Percentage") + xlab("") + ggtitle("B") + 
+  theme(plot.title = element_text(face = "bold", size = 32, hjust = 0), 
+        axis.title = element_text(face = "bold"), 
+        axis.text.y = element_text(size = 8))
 
 
-
+grid.arrange(lesionA, lesionB, nrow = 2, ncol = 1)
 
 # If wanting to add a table and figure together
 #grid.arrange(test1, test2, nrow = 1, ncol = 2)
