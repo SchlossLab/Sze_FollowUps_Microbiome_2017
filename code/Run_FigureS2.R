61a8348106166bc7e5f546970c8b2bc8a0684ca2
marcsze <marcasze@gmail.com>
2017-01-05 11:27:35 -0500

    add taxonomy data read in and remove extra unneeded lines of code

--- code/Run_FigureS2.R
+++ code/Run_FigureS2.R
@@ -15,12 +15,15 @@ lesion_MDA_data_summary <- read.csv("results/tables/lesion_model_top_vars_MDA_Su
 lesion_MDA_full <- read.csv("results/tables/lesion_model_top_vars_MDA_full_data.csv", 
                             header = T, stringsAsFactors = F)
 
+tax <- read.delim('data/process/final.taxonomy', sep='\t', header=T, row.names=1)
 
 
-
-rownames(lesion_MDA_data_summary) <- lesion_MDA_data_summary$variable
-
-lesion_MDA_data_summary <- lesion_MDA_data_summary[rev(unique(lesion_MDA_full$variables)), ]
+# Convert taxa table to a data frame with columns for each taxonomic division
+tax_df <- data.frame(do.call('rbind', strsplit(as.character(tax$Taxonomy), ';')))
+rownames(tax_df) <- rownames(tax)
+colnames(tax_df) <- c("Domain", "Phyla", "Order", "Class", "Family", "Genus")
+tax_df <- as.data.frame(apply(tax_df, 2, function(x) gsub("\\(\\d{2}\\d?\\)", "", x)))
+rm(tax)
 
 ggplot(lesion_MDA_full, aes(factor(variables, 
                                    levels = rev(unique(lesion_MDA_full$variables)), 
