77c82f88ca17e4fdecb70ee23684b504610ebdef
marcsze <marcasze@gmail.com>
2017-01-09 16:20:24 -0500

    incorporate updated Fig 3

--- code/Run_Figure3.R
+++ code/Run_Figure3.R
@@ -21,14 +21,13 @@ lesion_auc_info <- read.csv("results/tables/auc_summary.csv",
 IF_model_roc <- read.csv("results/tables/IF_test_data_roc.csv", header = T)
 reduced_IF_model_roc <- read.csv("results/tables/reduced_IF_test_data_roc.csv", header = T)
 
-grid.arrange(
+roc_curve_graph <- grid.arrange(
   # Graph showing all variable and reduced variable lesion model
   filter(lesion_model_roc, run != "middle_roc") %>% 
     ggplot(aes(x = sensitivities, y = specificities)) + 
     geom_polygon(alpha = 0.25) + 
     geom_line(aes(group = run), size = 1.25, color = "black", alpha = 0.5) + 
-    geom_polygon(data = filter(reduced_lesion_model_roc, run != "full_roc"), 
-                 aes(y = specificities), alpha = 0.25, fill = "red") + 
+    geom_polygon(data = filter(reduced_lesion_model_roc, run != "full_roc"), alpha = 0.25, fill = "red") + 
     geom_line(data = filter(reduced_lesion_model_roc, run != "full_roc"), 
               aes(group = run), size = 1.25, color = "red", alpha = 0.5) + 
     geom_line(data = filter(reduced_lesion_model_roc, run == "full_roc"), 
@@ -40,8 +39,7 @@ grid.arrange(
   ggplot(IF_model_roc, aes(x = sensitivities, y = specificities)) + 
     geom_polygon(alpha = 0.25) + 
     geom_line(aes(group = run), size = 1.25, color = "black", alpha = 0.5) + 
-    geom_polygon(data = filter(reduced_IF_model_roc, run != "full_roc"), 
-                 aes(y = specificities), alpha = 0.25, fill = "red") + 
+    geom_polygon(data = filter(reduced_IF_model_roc, run != "full_roc"), alpha = 0.25, fill = "red") + 
     geom_line(data = filter(reduced_IF_model_roc, run != "full_roc"), 
               aes(group = run), size = 1.25, color = "red", alpha = 0.5) + 
     geom_line(data = filter(reduced_IF_model_roc, run == "full_roc"), 
@@ -52,7 +50,7 @@ grid.arrange(
  
 
 # Save graph image as a pdf
-ggsave(file = "results/figures/Figure3.pdf", roc_curve_graph, 
+ggsave(file = "results/figures/Figure3.pdf", roc_curve_graph, device = "pdf", 
        width=8, height = 8, dpi = 300)
 
 
