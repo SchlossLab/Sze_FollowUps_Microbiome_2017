35dd12fb9f1dc3d04e75f3c844701997bf155bfe
marcsze <marcasze@gmail.com>
2017-02-19 14:53:40 -0500

    add italics and order

--- code/Run_FigureS2.R
+++ code/Run_FigureS2.R
@@ -5,13 +5,14 @@
 #Load needed code and packages
 source('code/functions.R')
 
-loadLibs(c("dplyr", "tidyr", "ggplot2", "reshape2", "gridExtra", "scales", "wesanderson", "knitr", "rmarkdown"))
+loadLibs(c("dplyr", "tidyr", "ggplot2", "reshape2", 
+           "gridExtra", "scales", "wesanderson"))
 
 #Read data needed
-lesion_MDA_data_summary <- read.csv("results/tables/lesion_model_top_vars_MDA_Summary.csv", 
+lesion_MDA_data_summary <- read.csv("results/tables/reduced_lesion_model_top_vars_MDA_Summary.csv", 
                             header = T, stringsAsFactors = F)
 
-lesion_MDA_full <- read.csv("results/tables/lesion_model_top_vars_MDA_full_data.csv", 
+lesion_MDA_full <- read.csv("results/tables/reduced_lesion_model_top_vars_MDA.csv", 
                             header = T, stringsAsFactors = F)
 
 tax <- read.delim('data/process/final.taxonomy', sep='\t', header=T, row.names=1)
@@ -25,26 +26,39 @@ colnames(tax_df) <- c("Domain", "Phyla", "Order", "Class", "Family", "Genus")
 tax_df <- as.data.frame(apply(tax_df, 2, function(x) gsub("\\(\\d{2}\\d?\\)", "", x)))
 rm(tax)
 
+lesion_MDA_data_summary <- lesion_MDA_data_summary[order(lesion_MDA_data_summary$mean_MDA, decreasing = TRUE), ]
+
+
 # Pull OTUs that are only in the MDA data
-OTU_IDs <- unique(filter(lesion_MDA_full, variables != "fit_result")[, "variables"])
+OTU_IDs <- unique(filter(lesion_MDA_data_summary, variable != "fit_result")[, "variable"])
 select_tax_df <- tax_df[OTU_IDs, ]
 low_tax_ID <- gsub("_unclassified", "", createTaxaLabeller(select_tax_df))
 
 # create labels for factor values with low taxonomy
-graph_labels <- c("FIT", paste(names(low_tax_ID), " (", low_tax_ID, ")", sep = ""))
+graph_labels <- c("FIT", paste(gsub("2", "", low_tax_ID), " (", names(low_tax_ID), ")", sep = ""))
+OTU_names <- names(low_tax_ID)
+
+test <- c()
+for(i in 1:length(low_tax_ID)){
+  
+  test <- c(test, bquote(paste(italic(.(low_tax_ID[i])) ~ "(", .(OTU_names[i]), ")", sep = "")))
+}
+
+test2 <- do.call(expression, test)
 
 # align occurance data with MDA data
 rownames(occurance_data) <- occurance_data$Variable
-occurance_data <- occurance_data[unique(lesion_MDA_full$variables), ]
+occurance_data <- occurance_data[unique(lesion_MDA_data_summary$variable), ]
 
 
 # create part A plot
 lesionA <- ggplot(lesion_MDA_full, aes(factor(variables, 
-                                   levels = rev(unique(lesion_MDA_full$variables)), 
+                                   levels = rev(unique(lesion_MDA_data_summary$variable)), 
                                    labels = rev(graph_labels)), 
                                    log10(value))) + 
   geom_point(aes(color = variable)) + stat_summary(fun.y = "median", colour = "black", geom = "point", size = 2.5) + 
   coord_flip() + theme_bw() + ylab("Log10 MDA") + xlab("Variable") +  ggtitle("A") + 
+  scale_x_discrete(labels = rev(c("FIT", test2))) + 
   theme(plot.title = element_text(face = "bold", size = 32, hjust = 0), 
         legend.position = "none", 
         axis.title = element_text(face = "bold"), 
