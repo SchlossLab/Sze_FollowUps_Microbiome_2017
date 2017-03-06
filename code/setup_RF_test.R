4985a68e9c4d68b236d8cc6161868b333299b36f
marcsze <marcasze@gmail.com>
2017-03-06 18:52:38 -0500

    change to data/process

--- code/setup_RF_test.R
+++ code/setup_RF_test.R
@@ -12,10 +12,10 @@ loadLibs(c("dplyr", "caret","scales", "doMC"))
 shared <- read.delim('data/process/final.0.03.subsample.shared', 
   header=T, sep='\t') %>% select(Group, contains("Otu0"))
 
-metaI <- read.csv("results/tables/mod_metadata/metaI_final.csv", 
+metaI <- read.csv("data/process/mod_metadata/metaI_final.csv", 
   stringsAsFactors = F, header = T) 
 
-good_metaf <- read.csv('results/tables/mod_metadata/good_metaf_final.csv', 
+good_metaf <- read.csv('data/process/mod_metadata/good_metaf_final.csv', 
                        header = T, stringsAsFactors = F) %>% select(initial)
 
 metaI <- filter(metaI, !(sample %in% good_metaf$initial))
@@ -64,7 +64,7 @@ test_data <- cbind(lesion = factor(metaI$lesion,
   levels = c(0, 1), labels = c("No", "Yes")), test_data)
 
 # Write data table for future use
-write.csv(test_data, "results/tables/full_test_data.csv")
+write.csv(test_data, "data/process/tables/full_test_data.csv")
 
 
 #################################################################################
