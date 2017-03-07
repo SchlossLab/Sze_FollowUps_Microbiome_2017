46febb85f1b1107b3bb5b9fc64d7d6ecd80e2dc2
marcsze <marcasze@gmail.com>
2017-03-07 12:24:57 -0500

    remove FIT from figure

--- code/Run_Figure1.R
+++ code/Run_Figure1.R
@@ -37,41 +37,20 @@ differences_graph <- grid.arrange(
     theme(axis.title = element_text(face="bold", hjust = 0.5), 
           plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"), 
           legend.title = element_text(face="bold"), 
-          legend.position = c(0.75, 0.2), 
+          legend.position = c(0.75, 0.20), 
           title = element_text(face="bold", hjust = 0), 
           panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank()), 
-  
-  # Difference from fit between adenoma and cancer
-  ggplot(difference_table_treatment, 
-         aes(factor(dx, levels = c("adenoma", "cancer"), labels = c("Adenoma", "Carcinoma")), 
-             fit_difference*-1, group = 1)) + 
-    geom_jitter(aes(color=Dx_Bin), width = 0.3, size = 4, alpha = 0.5) + 
-    stat_summary(fun.data = "mean_cl_boot", colour = "black", size = 1) + 
-    stat_summary(fun.y = mean, colour = "black", geom = "line") + 
-    scale_color_manual(name = "Lesion Type", 
-                       values = c('#00FFFF', '#0000FF', '#F1BB7B'), 
-                       breaks = c("adenoma", "adv_adenoma", "cancer"), 
-                       labels = c("Adenoma", "SRN", "Carcinoma")) + 
-    ylab("Change in FIT from Follow Up to Initial") + 
-    xlab("") + theme_bw() + ggtitle("B") + 
-    theme(axis.title = element_text(face="bold", hjust = 0.5), 
-          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"), 
-          legend.title = element_text(face="bold"), 
-          legend.position = "none", 
-          title = element_text(face="bold", hjust = 0), 
-          panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank()), 
+          panel.grid.minor = element_blank()) + coord_fixed(ratio = 1.8), 
   
   # Adenoma Only initial and follow up
   mutate(thetayc_adn_IF, samples = factor(samples, levels = c("follow_up", "initial"), labels = c("Follow Up", "Initial"))) %>% 
   ggplot(aes(x=NMDS1, y=NMDS2)) + geom_point(aes(color=samples)) + 
-    theme_bw() + coord_equal() + ggtitle("C") + ylim(-0.7, 0.7) + xlim(-0.7, 0.7) + 
+    theme_bw() + coord_equal() + ggtitle("B") + ylim(-0.7, 0.7) + xlim(-0.7, 0.7) + 
     stat_ellipse(aes(group = samples, color = samples, fill = samples), 
                  alpha = 0.25, geom = "polygon", show.legend = FALSE) + 
     scale_color_manual(values = c('#483D8B', '#00BFFF')) + scale_fill_manual(values = c('#483D8B', '#00BFFF')) +
     theme(plot.title = element_text(face = "bold", hjust = 0), 
-          legend.position = c(0.20, 0.12), 
+          legend.position = c(0.25, 0.12), 
           legend.title = element_blank(), 
           legend.key = element_blank(), 
           legend.background = element_rect(color = "black"), 
@@ -85,17 +64,18 @@ differences_graph <- grid.arrange(
                           labels = c("Follow Up", "Initial"))) %>%
   ggplot(aes(x=NMDS1, y=NMDS2)) + 
     geom_point(aes(color=samples)) + theme_bw() + 
-    coord_equal() + ggtitle("D") + ylim(-0.7, 0.7) + xlim(-0.7, 0.7) +
+    coord_equal() + ggtitle("C") + ylim(-0.7, 0.7) + xlim(-0.7, 0.7) +
     stat_ellipse(aes(group = samples, color = samples, fill = samples), 
                  alpha = 0.25, geom = "polygon", show.legend = FALSE) + 
     scale_color_manual(values = c('#FD6467', '#F1BB7B')) + scale_fill_manual(values = c('#FD6467', '#F1BB7B')) + 
     theme(plot.title = element_text(face = "bold", hjust = 0), 
-          legend.position = c(0.20, 0.12), 
+          legend.position = c(0.25, 0.12), 
           legend.title = element_blank(), 
           legend.background = element_rect(color = "black"), 
           panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank()) + 
-    coord_fixed(ratio = 1.4), ncol = 2, nrow = 2)
+    coord_fixed(ratio = 1.40), layout_matrix = cbind(c(1, 2), c(1, 3)))
+
 
 
 ggsave(file = "results/figures/Figure1.pdf", differences_graph, 
@@ -107,7 +87,26 @@ ggsave(file = "results/figures/Figure1.pdf", differences_graph,
 
 
 
-
+# Difference from fit between adenoma and cancer
+#ggplot(difference_table_treatment, 
+#       aes(factor(dx, levels = c("adenoma", "cancer"), labels = c("Adenoma", "Carcinoma")), 
+#           fit_difference*-1, group = 1)) + 
+#  geom_jitter(aes(color=Dx_Bin), width = 0.3, size = 4, alpha = 0.5) + 
+#  stat_summary(fun.data = "mean_cl_boot", colour = "black", size = 1) + 
+#  stat_summary(fun.y = mean, colour = "black", geom = "line") + 
+#  scale_color_manual(name = "Lesion Type", 
+#                     values = c('#00FFFF', '#0000FF', '#F1BB7B'), 
+ #                    breaks = c("adenoma", "adv_adenoma", "cancer"), 
+  #                   labels = c("Adenoma", "SRN", "Carcinoma")) + 
+#  ylab("Change in FIT from Follow Up to Initial") + 
+#  xlab("") + theme_bw() + ggtitle("B") + 
+#  theme(axis.title = element_text(face="bold", hjust = 0.5), 
+#        plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"), 
+#        legend.title = element_text(face="bold"), 
+#        legend.position = "none", 
+#        title = element_text(face="bold", hjust = 0), 
+#        panel.grid.major = element_blank(), 
+#        panel.grid.minor = element_blank())
 
 
 
