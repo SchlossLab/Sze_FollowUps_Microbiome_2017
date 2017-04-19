24f48abc06bbfddbb2e0aee5295c055c77a307ed
marcsze <marcasze@gmail.com>
2017-04-19 15:18:24 -0400

    future research fig update

--- code/graph_check_crc_probs_increase.R
+++ code/graph_check_crc_probs_increase.R
@@ -23,8 +23,6 @@ oral_p <- ggplot(residents, aes(factor(sampleType,
   ggtitle("A") + theme_bw() +  xlab("") + ylab("% Relative Abundance") + 
   stat_summary(aes(group = probs_increase), fun.y = median, 
                colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "point", size = 4.5) + 
-  stat_summary(aes(group = probs_increase), fun.y = median, 
-               colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "line", size = 1) + 
   coord_cartesian(ylim = c(0, 15)) + 
   theme(plot.title = element_text(face = "bold", hjust = -0.08, size = 20), 
         legend.position = c(0.9, 0.4)) + 
@@ -42,16 +40,46 @@ non_oral_p <- ggplot(residents, aes(factor(sampleType,
   ggtitle("B") + theme_bw() +  xlab("") + ylab("% Relative Abundance") + 
   stat_summary(aes(group = probs_increase), fun.y = median, 
                colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "point", size = 4.5) + 
-  stat_summary(aes(group = probs_increase), fun.y = median, 
-               colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "line", size = 1) + 
   coord_cartesian(ylim = c(0, 100)) + 
   theme(plot.title = element_text(face = "bold", hjust = -0.09, size = 20), 
         legend.position = "none") + 
   annotate("text", 
            label = paste("Non-Oral Pathogens"),x = 1.5, y = 100, size = 4)
+
+
+crc_only_group <- ggplot(residents, aes(factor(sampleType, 
+                                           levels = c("initial", "followup"), 
+                                           labels = c("Pre", "Post")), crc_only)) + 
+  geom_jitter(aes(color = probs_increase), width = 0.1) + 
+  scale_color_manual(name = "Carcinoma\nProbabiliy\nIncrease", 
+                     values = c('#8968CD', '#FF00FF')) + 
+  ggtitle("B") + theme_bw() +  xlab("") + ylab("% Relative Abundance") + 
+  stat_summary(aes(group = probs_increase), fun.y = median, 
+               colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "point", size = 4.5) + 
+  coord_cartesian(ylim = c(0, 75)) + 
+  theme(plot.title = element_text(face = "bold", hjust = -0.09, size = 20), 
+        legend.position = "none") + 
+  annotate("text", 
+           label = paste("Carcinoma Higher"),x = 1.5, y = 75, size = 4)
+
+
+normal_only_group <- ggplot(residents, aes(factor(sampleType, 
+                                               levels = c("initial", "followup"), 
+                                               labels = c("Pre", "Post")), normal_only)) + 
+  geom_jitter(aes(color = probs_increase), width = 0.1) + 
+  scale_color_manual(name = "Carcinoma\nProbabiliy\nIncrease", 
+                     values = c('#8968CD', '#FF00FF')) + 
+  ggtitle("C") + theme_bw() +  xlab("") + ylab("% Relative Abundance") + 
+  stat_summary(aes(group = probs_increase), fun.y = median, 
+               colour = c('#8968CD', '#8968CD', '#FF00FF', '#FF00FF'), geom = "point", size = 4.5) + 
+  coord_cartesian(ylim = c(0, 75)) + 
+  theme(plot.title = element_text(face = "bold", hjust = -0.09, size = 20), 
+        legend.position = "none") + 
+  annotate("text", 
+           label = paste("Normal Higher"),x = 1.5, y = 75, size = 4)
   
   
-combined_graph <- grid.arrange(oral_p, non_oral_p, ncol = 1)
+combined_graph <- grid.arrange(oral_p, crc_only_group, normal_only_group, ncol = 1)
 
 ggsave(file = "results/figures/FigureS4.pdf", combined_graph, 
        width = 6, height = 8, dpi = 300)
