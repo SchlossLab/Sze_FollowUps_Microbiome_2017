a9d5392b21ebb75d5c14ad9cd2c8e2fe9a84d854
marcsze <marcasze@gmail.com>
2017-05-02 20:43:09 -0400

    remove histogram of pvalue graph

--- code/Run_FigureS1.R
+++ code/Run_FigureS1.R
@@ -1,5 +1,5 @@
 ### Figure S1
-### Distribution of P-values from Paired Wilcoxson Analysis
+### ROC Curves summary for three models used
 ## Marc Sze
 
 #Load needed code and packages
@@ -8,17 +8,53 @@ source('code/functions.R')
 loadLibs(c("dplyr", "tidyr", "ggplot2", "reshape2", "gridExtra", "scales", "wesanderson", "knitr", "rmarkdown"))
 
 #Read data needed
-paired_table <- read.csv("data/process/tables/OTU_paired_wilcoxson_test.csv", header = T, stringsAsFactors = F)
+adn_model_roc <- read.csv("data/process/tables/adn_reduced_test_data_roc.csv", header = T)
+srn_model_roc <- read.csv("data/process/tables/srn_reduced_test_data_roc.csv", header = T)
+crc_model_roc <- read.csv("data/process/tables/crc_reduced_test_data_roc.csv", header = T)
 
-label_names <- c("Adenoma", "Advanced Adenoma", "Carcinoma")
-names(label_names) <- c("adn", "srn", "crc")
 
-pvalue_distribution_paired <- ggplot(paired_table, aes(x=BH_corrected)) + geom_histogram() + 
-  geom_vline(aes(xintercept = 0.05, color = "red"), linetype = 2, size = 1) + 
-  facet_wrap(~factor(analysis, levels = c("adn", "srn", "crc")), 
-             labeller = as_labeller(label_names)) + theme_bw() + 
-  xlab("Benjamini-Hochberg Corrected P-values") + ylab("Counts") + 
-  theme(legend.position = "none")
+# Create the graph
 
-ggsave(file = "results/figures/FigureS1.pdf", pvalue_distribution_paired, 
-       width=12, height = 6, dpi = 300)
+rocs_graph <- grid.arrange(
+  # Adenoma ROC curve information
+  filter(adn_model_roc, run != "middle_roc" & run != "full_roc") %>% 
+    ggplot(aes(x = sensitivities, y = specificities)) + 
+    geom_polygon(data = filter(adn_model_roc, run != "full_roc"), alpha = 0.5, fill = '#76EE00') + 
+    geom_line(data = filter(adn_model_roc, run != "full_roc"), 
+              aes(group = run), size = 1.25, color = '#76EE00') + 
+    geom_line(data = filter(adn_model_roc, run == "full_roc"), 
+              size = 1.5, color = '#006400') + 
+    scale_x_continuous(trans = "reverse") + theme_bw() + 
+    ggtitle("A") + xlab("Sensitivity") + ylab("Specificity") + 
+    theme(plot.title = element_text(face= "bold", hjust = -0.19, size = 20), 
+          axis.title = element_text(face = "bold")), 
+  
+  # SRN ROC curve information
+  filter(srn_model_roc, run != "middle_roc" & run != "full_roc") %>% 
+    ggplot(aes(x = sensitivities, y = specificities)) + 
+    geom_polygon(data = filter(srn_model_roc, run != "full_roc"), alpha = 0.5, fill = '#F0E68C') + 
+    geom_line(data = filter(srn_model_roc, run != "full_roc"), 
+              aes(group = run), size = 1.25, color = '#F0E68C') + 
+    geom_line(data = filter(srn_model_roc, run == "full_roc"), 
+              size = 1.5, color = '#EEC900') + 
+    scale_x_continuous(trans = "reverse") + theme_bw() +  
+    ggtitle("B") + xlab("Sensitivity") + ylab("Specificity") + 
+    theme(plot.title = element_text(face= "bold", hjust = -0.19, size = 20), 
+          axis.title = element_text(face = "bold")), 
+  
+  # CRC ROC curve information
+  filter(crc_model_roc, run != "middle_roc" & run != "full_roc") %>% 
+    ggplot(aes(x = sensitivities, y = specificities)) + 
+    geom_polygon(data = filter(crc_model_roc, run != "full_roc"), alpha = 0.5, fill = '#FFB6C1') + 
+    geom_line(data = filter(crc_model_roc, run != "full_roc"), 
+              aes(group = run), size = 1.25, color = '#FFB6C1') + 
+    geom_line(data = filter(crc_model_roc, run == "full_roc"), 
+              size = 1.5, color = '#DC143C') + 
+    scale_x_continuous(trans = "reverse") + theme_bw() +  
+    ggtitle("C") + xlab("Sensitivity") + ylab("Specificity") + 
+    theme(plot.title = element_text(face= "bold", hjust = -0.19, size = 20), 
+          axis.title = element_text(face = "bold")))
+
+
+ggsave(file = "results/figures/FigureS1.tiff", rocs_graph, 
+       width=4, height = 8, dpi = 300)
---
+++ code/old/Run_FigureS1.R
@@ -0,0 +1,24 @@
+### Figure S1
+### Distribution of P-values from Paired Wilcoxson Analysis
+## Marc Sze
+
+#Load needed code and packages
+source('code/functions.R')
+
+loadLibs(c("dplyr", "tidyr", "ggplot2", "reshape2", "gridExtra", "scales", "wesanderson", "knitr", "rmarkdown"))
+
+#Read data needed
+paired_table <- read.csv("data/process/tables/OTU_paired_wilcoxson_test.csv", header = T, stringsAsFactors = F)
+
+label_names <- c("Adenoma", "Advanced Adenoma", "Carcinoma")
+names(label_names) <- c("adn", "srn", "crc")
+
+pvalue_distribution_paired <- ggplot(paired_table, aes(x=BH_corrected)) + geom_histogram() + 
+  geom_vline(aes(xintercept = 0.05, color = "red"), linetype = 2, size = 1) + 
+  facet_wrap(~factor(analysis, levels = c("adn", "srn", "crc")), 
+             labeller = as_labeller(label_names)) + theme_bw() + 
+  xlab("Benjamini-Hochberg Corrected P-values") + ylab("Counts") + 
+  theme(legend.position = "none")
+
+ggsave(file = "results/figures/FigureS1.pdf", pvalue_distribution_paired, 
+       width=12, height = 6, dpi = 300)
