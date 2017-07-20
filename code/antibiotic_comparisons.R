1517db03b0d61744d9450ab4ba20e82ddcb56f5e
marcsze <marcasze@gmail.com>
2017-07-19 21:15:23 -0400

    completed code without read outs

--- code/antibiotic_comparisons.R
+++ code/antibiotic_comparisons.R
@@ -5,7 +5,7 @@
 ###Load needed Libraries and functions
 source('code/functions.R')
 
-loadLibs(c("dplyr", "tidyr"))
+loadLibs(c("dplyr", "tidyr", "ggplot2", "gridExtra"))
 
 
 # Read in data tables
@@ -52,34 +52,109 @@ working_beta <- as.data.frame(initial_samples) %>%
 
 
 ## Make comparisons of surgery for adenoma and advanced adenoma within groups
-working_beta %>% filter(Dx_Bin == "adenoma") %>% group_by(Surgery) %>% 
+surg_non_crc_summary <- working_beta %>% filter(Dx_Bin == "adenoma") %>% group_by(Surgery) %>% 
   summarise(median_value = median(distance), 
-            q25 = quantile(distance)["25%"], q75 = quantile(distance)["75%"])
+            q25 = quantile(distance)["25%"], q75 = quantile(distance)["75%"]) %>% 
+  mutate(dx = c("adenoma", "adenoma")) %>% 
+  bind_rows(working_beta %>% filter(Dx_Bin == "adv_adenoma") %>% group_by(Surgery) %>% 
+              summarise(median_value = median(distance), 
+                        q25 = quantile(distance)["25%"], q75 = quantile(distance)["75%"]) %>% 
+              mutate(dx = c("adv_adenoma", "adv_adenoma")))
 
-wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
-            filter(working_beta, Dx_Bin == "adenoma" & Surgery == "N")[, "distance"])
+non_crc_surg_pvalues <- data_frame(pvalues = c(wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
+            filter(working_beta, Dx_Bin == "adenoma" & Surgery == "N")[, "distance"])$p.value, 
+            wilcox.test(filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"], 
+            filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "N")[, "distance"])$p.value)) %>% 
+  mutate(BH = p.adjust(pvalues, method = "BH"))
 
-working_beta %>% filter(Dx_Bin == "adv_adenoma") %>% group_by(Surgery) %>% 
-  summarise(median_value = median(distance), 
-            q25 = quantile(distance)["25%"], q75 = quantile(distance)["75%"])
-
-wilcox.test(filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"], 
-            filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "N")[, "distance"])
 
 
 ## Make comparisons of only surgery changes based on diagnosis
+surg_only_comparisons <- data_frame(
+  pvalues = c(
+    wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
+                filter(working_beta, Dx_Bin == "cancer" & Surgery == "Y")[, "distance"])$p.value, 
+    wilcox.test(filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"], 
+                filter(working_beta, Dx_Bin == "cancer" & Surgery == "Y")[, "distance"])$p.value, 
+    wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
+                filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"])$p.value)) %>% 
+  mutate(BH = p.adjust(pvalues, method = "BH"), comparison = c("adnVcrc", "srnVcrc", "adnVsrn"))
 
-wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
-            filter(working_beta, Dx_Bin == "cancer" & Surgery == "Y")[, "distance"])
-
-wilcox.test(filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"], 
-            filter(working_beta, Dx_Bin == "cancer" & Surgery == "Y")[, "distance"])
-
-wilcox.test(filter(working_beta, Dx_Bin == "adenoma" & Surgery == "Y")[, "distance"], 
-            filter(working_beta, Dx_Bin == "adv_adenoma" & Surgery == "Y")[, "distance"])
 
 
 # Want to graph both sections and add a supplemental figure 
 
+surgery_only_plot <- working_beta %>% filter(Surgery == "Y") %>% 
+ggplot(aes(factor(Dx_Bin, levels = c("adenoma", "adv_adenoma", "cancer"), 
+                  labels = c("Adenoma", "Advanced\nAdenoma", "Carcinoma")), distance, group = 1)) + 
+  geom_jitter(aes(color=Dx_Bin), width = 0.3, size = 4, alpha = 0.5) + 
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median, 
+               colour = "black", geom = "crossbar", size = 0.5, width = 0.5) + 
+  scale_color_manual(name = "Lesion Type", 
+                     values = c('#228B22', '#FFD700', '#DC143C'), 
+                     breaks = c("adenoma", "adv_adenoma", "cancer"), 
+                     labels = c("Adenoma", "Advanced\nAdenoma", "Carcinoma")) + 
+  coord_cartesian(ylim = c(0, 1)) + ylab(expression(paste(theta[YC], " Distance"))) + 
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1.0), 
+                     labels = c("0.00", "0.25", "0.50", "0.75", "1.00"),limits = c(-0.06, 1.06)) + 
+  xlab("") + theme_bw() + ggtitle("A") + 
+  theme(axis.title = element_text(face="bold", hjust = 0.5), 
+        axis.text.x = element_text(size = 12), 
+        legend.title = element_text(face="bold"), 
+        legend.position = "none", 
+        plot.title = element_text(face="bold", hjust = -0.065, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
 
 
+adenoma_surg_diff <- working_beta %>% filter(Dx_Bin == "adenoma") %>% 
+  ggplot(aes(factor(Surgery, levels = c("Y", "N"), labels = c("Yes", "No")), distance)) + 
+  geom_jitter(aes(color = Surgery), width = 0.3, size = 4, alpha = 0.5) + 
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median, 
+               colour = "black", geom = "crossbar", size = 0.5, width = 0.5) + 
+  scale_color_manual(name = "Surgery Received", 
+                     values = c('#00EE76', '#228B22'), 
+                     breaks = c("Y", "N"), 
+                     labels = c("Yes", "No")) + 
+  coord_cartesian(ylim = c(0, 1)) + ylab(expression(paste(theta[YC], " Distance"))) + 
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1.0), 
+                     labels = c("0.00", "0.25", "0.50", "0.75", "1.00"),limits = c(-0.06, 1.06)) + 
+  xlab("") + theme_bw() + ggtitle("B") + 
+  theme(axis.title = element_text(face="bold", hjust = 0.5), 
+        axis.text.x = element_text(size = 12), 
+        legend.title = element_text(face="bold"), 
+        legend.position = "none", 
+        plot.title = element_text(face="bold", hjust = -0.065, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
+
+
+
+adv_adenoma_surg_diff <- working_beta %>% filter(Dx_Bin == "adv_adenoma") %>% 
+  ggplot(aes(factor(Surgery, levels = c("Y", "N"), labels = c("Yes", "No")), distance)) + 
+  geom_jitter(aes(color = Surgery), width = 0.3, size = 4, alpha = 0.5) + 
+  stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median, 
+               colour = "black", geom = "crossbar", size = 0.5, width = 0.5) + 
+  scale_color_manual(name = "Surgery Received", 
+                     values = c('#CDAD00', '#8B7500'), 
+                     breaks = c("Y", "N"), 
+                     labels = c("Yes", "No")) + 
+  coord_cartesian(ylim = c(0, 1)) + ylab(expression(paste(theta[YC], " Distance"))) + 
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1.0), 
+                     labels = c("0.00", "0.25", "0.50", "0.75", "1.00"),limits = c(-0.06, 1.06)) + 
+  xlab("") + theme_bw() + ggtitle("C") + 
+  theme(axis.title = element_text(face="bold", hjust = 0.5), 
+        axis.text.x = element_text(size = 12), 
+        legend.title = element_text(face="bold"), 
+        legend.position = "none", 
+        plot.title = element_text(face="bold", hjust = -0.065, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
+
+
+test <- grid.arrange(surgery_only_plot, adenoma_surg_diff, adv_adenoma_surg_diff, 
+                     layout_matrix = cbind(c(1, 2), c(1, 3)))
+
+
+ggsave(file = "results/figures/FigureS1.pdf", test, 
+       width=10, height = 8.5, dpi = 300)
