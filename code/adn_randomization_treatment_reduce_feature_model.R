b68861662ea647737e6dbf20cd4f438ecab96f3c
marcsze <marcasze@gmail.com>
2017-09-10 17:59:59 -0400

    complete code to generate needed reduced randomization model data

--- code/adn_randomization_treatment_reduce_feature_model.R
+++ code/adn_randomization_treatment_reduce_feature_model.R
@@ -11,15 +11,8 @@ loadLibs(c("randomForest", "dplyr", "scales", "caret"))
 #Load needed data
 load("exploratory/adn_randomized_treatment_model.RData")
 
-# Get the MDA counts from most to least
-test_imp <- varImp(test_tune_list[[1]], scale = F)$importance %>% 
-  mutate(otu = rownames(.)) %>% 
-  arrange(desc(Overall))
-  
-
-#Create data table with only reduced features (impvars only)
-vars_to_keep <- slice(test_imp, 1:10)[, "otu"]
-test_data_imps <- select(test_data, lesion, one_of(vars_to_keep$otu))
+rm(good_metaf, good_metaf_select, shared, test_data, 
+   fitControl, i, nzv)
 
 
 #################################################################################
@@ -55,7 +48,15 @@ max_auc <- c()
 
 for(i in 1:100){
   
-  #Get test data
+  # Get the MDA counts from most to least
+  test_imp <- varImp(test_tune_list[[i]], scale = F)$importance %>% 
+    mutate(otu = rownames(.)) %>% 
+    arrange(desc(Overall))
+  
+  #Create data table with only reduced features (impvars only)
+  vars_to_keep <- slice(test_imp, 1:10)[, "otu"]
+  test_data_imps <- select(stored_data[[i]], lesion, one_of(vars_to_keep$otu))
+  
   train_test_data <- test_data_imps
   stored_aucs <- c()
   
